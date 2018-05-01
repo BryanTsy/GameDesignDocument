3e46ef236101a1f2f7e9738c568388c8316c208b
WILSON TO WEE SEN <100076027@students.swinburne.edu.my>
2018-05-01 21:21:10 +0800

    cs added

---
+++ Screens/ConfirmExit.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlappyBird.Screens
+
+
+{
+   class ConfirmExit 
+    { 
+
+        public ConfirmExit()
+        {
+            Statics.GAME_STATE = Statics.STATE.Loading;
+        }
+
+      
+
+
+
+
+    }
+}
